aff9dcf91753ba0eca5a1099336d426d0a579110
Daipayan Mukherjee <daisatyr09@gmail.com>
2019-07-04 19:18:25 +0530

    first

---
+++ precache-manifest.d8d402f7314a351236b75892b1bf38e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1964d5680a5f1e63676bd9626b84a70f",
+    "url": "/playment_frontend_test/index.html"
+  },
+  {
+    "revision": "bb3a771d00ea37b7049a",
+    "url": "/playment_frontend_test/static/css/main.4fcd75ec.chunk.css"
+  },
+  {
+    "revision": "8088e5799732f8208669",
+    "url": "/playment_frontend_test/static/js/2.a3bcf32d.chunk.js"
+  },
+  {
+    "revision": "bb3a771d00ea37b7049a",
+    "url": "/playment_frontend_test/static/js/main.e1c0f4db.chunk.js"
+  },
+  {
+    "revision": "d9e0f3462c3fdf38c7a9",
+    "url": "/playment_frontend_test/static/js/runtime~main.efa16e6a.js"
+  },
+  {
+    "revision": "8f62010aa6bd29bdabeca9b401cdea3c",
+    "url": "/playment_frontend_test/static/media/thank.8f62010a.svg"
+  },
+  {
+    "revision": "aebbf662882654eb9aaa681827e32f31",
+    "url": "/playment_frontend_test/static/media/topContributors.aebbf662.svg"
+  },
+  {
+    "revision": "78c3087924b87e44e025f21f1f58e71b",
+    "url": "/playment_frontend_test/static/media/weather.78c30879.svg"
+  }
+]);
